c8b69b77e8182295a2b86839e5babf755f9a99fd
self4m <57866775+self4m@users.noreply.github.com>
2025-05-12 11:00:15 +0800

    修改接口注解的错误

--- src/main/java/org/self4m/ollama_java/service/ChatService.java
+++ src/main/java/org/self4m/ollama_java/service/ChatService.java
@@ -1,9 +1,7 @@
 package org.self4m.ollama_java.service;
 
-import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 
-@Service
 public interface ChatService {
     String getResponse(String text);
 
